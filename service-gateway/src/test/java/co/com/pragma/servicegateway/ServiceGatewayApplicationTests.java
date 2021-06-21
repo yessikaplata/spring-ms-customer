c93b194cc937ee23956caaa0ceeba95316401329
yessikaplata <85967157+yessikaplata@users.noreply.github.com>
2021-06-21 13:04:56 -0500

    proyecto gateway

---
+++ service-gateway/src/test/java/co/com/pragma/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.pragma.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
